7b61806e82dd96a56d51cd050fe99a65a18c1b4f
Adarsh-20021209 <chaubeyadarsh579@gmail.com>
2025-12-24 09:47:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Simple animation on scroll
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach(section => {
+    const position = section.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
+
+    if (position < screenPosition) {
+      section.style.opacity = "1";
+      section.style.transform = "translateY(0)";
+    }
+  });
+});
